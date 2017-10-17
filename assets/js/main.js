25f7f84ff9582a9ac141621c6bfa9da057456263
izaweslie <izaweslie@gmail.com>
2017-10-16 20:01:40 -0500

    Single button user upload and slide div
    Still some issues to work out with the div that the file loads into and getting the googlevision api call to return into the overlay div

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,36 @@
 //Vision Api Logic
 var api_key = 'AIzaSyBY93fja8yxM9not6Nrd2v6NsRgNpJ4ZvM';
 
+var API_URL = 'https://api.shutterstock.com/v2';
+var clientId = "cc7ea-f2b80-dd2ff-22097-cbcae-44883";//$('input[name=client_id]').val();
+var clientSecret = "dd14b-4447f-39c62-1052f-cb9cb-24460";//$('input[name=client_secret]').val();
+var authorization = 'Basic ' + window.btoa(clientId + ':' + clientSecret);
+var shutterImageUrl;
+
+$(function () {
+    $(".test").change(function () {
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = imageIsLoaded;
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+}); 
+
+function imageIsLoaded(e) {
+    $('#myImg').attr('src', e.target.result);
+};
+function chooseFile() {
+  document.getElementById("fileInput").click();
+}
+function resultsTimer() {
+  $("#loader").hide();
+  $("#results").fadeIn(100);
+}
+setTimeout(resultsTimer, 3000);
+
+
+
 //Handles the User image upload.
 function uploadFiles(event) {
   console.log('uploaded file')
@@ -22,21 +52,36 @@ function processFile(event) {
   sendFiletoCloudVision(encodedFile);
 }
 
+//Gets all the information from the returned json object
 function displayJSON(object){
   var labelArr = object.responses[0].labelAnnotations;
-  var string = "";
-  for (var i=0; i < labelArr.length; i++){
+  // var string = "";
+  // for (var i=0; i < labelArr.length; i++){
+  //   console.log(labelArr[i].description + " | " + parseInt(labelArr[i].score*100) + "% match");
+  //   $('#results').append('<p>'+labelArr[i].description + " | " + parseInt(labelArr[i].score*100) + "% match" + '</p>');
+  //   string += labelArr[i].description + ", "
+  // }
+  var apiString = "";
+  for (var i=0; i < 5; i++) {
     console.log(labelArr[i].description + " | " + parseInt(labelArr[i].score*100) + "% match");
     $('#results').append('<p>'+labelArr[i].description + " | " + parseInt(labelArr[i].score*100) + "% match" + '</p>');
-    string += labelArr[i].description + ", "
+    apiString += labelArr[i].description + "+"
   }
-  $('#results').append('<p>'+string+'</p>')
+  //$('#results').append('<p>'+apiString+'</p>')
+  // apiString = labelArr[0].description //+'+'+labelArr[1].description+'+'+labelArr[2].description;
+  // apiString = apiString.replace(" ", "+");
+  console.log("here is" + apiString)
+  search({query:labelArr[0].description+"+"+labelArr[1].description+"+"+labelArr[2].description+"+"+labelArr[3].description+"+"+labelArr[4].description});
 }
 
+//Sends the file to CloudVision
 function sendFiletoCloudVision(file){
   var type = 'LABEL_DETECTION';
   //This will currently only allow jpeg images
-  var content = file.replace("data:image/jpeg;base64,", "");
+  var fileType = file.split(',');
+  fileType = fileType[0] + ",";
+  console.log(fileType)
+  var content = file.replace(fileType, "");
   showImage(content)
     // Strip out the file prefix when you convert to json.
     var json = {
@@ -70,7 +115,7 @@ function sendFiletoCloudVision(file){
    
       success: function(data, textStatus, jqXHR) {
         displayJSON(data);
-        //console.log(data);
+        console.log(data);
         //console.log(textStatus)
         //console.log(jqXHR)
       },
@@ -81,15 +126,81 @@ function sendFiletoCloudVision(file){
       }
   });
 }
+function search(opts) {
+        console.log("look here" + opts);
+        var url = API_URL + '/images/search';
+        console.log(url);
+        $.ajax({
+          url: url,
+          data: opts,
+          headers: {
+            Authorization: authorization
+          }
+        })
+        .done(function(data) {
+
+
+           shutterImageUrl = data.data[0].assets.preview.url;
+          console.log(shutterImageUrl)
+          var shutterImage = $('<img style="height:100vh" src="' + shutterImageUrl + '"/>');
+           $('.received').append(shutterImage);
+          if (data.total_count === 0) {
+            console.log("none homie");
+            return;
+          }
+          else {
+            var shutterImageURL = data.data[0].assets.preview.url;
+            console.log(shutterImageURL)
+            var shutterImage = $('<img style="height:100vh" src="' + shutterImageURL + '"/>');
+             $('.received').append(shutterImage);
+           }
+        })
+        .fail(function(xhr, status, err) {
+          alert('Failed to retrieve ' + mediaType + ' search results:\n' + JSON.stringify(xhr.responseJSON, null, 2));
+        });
+        return;
+      }
 
 function showImage(base64){
-  var image = $('<img style="width: 400px;" src="data:image/jpeg;base64, '+ base64 +'" />');
-  $('#uploaded').append(image);
+  var image = $('<img style="height:100vh" src="data:image/jpeg;base64, '+ base64 +'" />');
+  $('.uploaded').append(image);
 }
 
-$('#submit').on('click', function(event){
+//
+$('#uploadImage').on('click', function(event){
   uploadFiles(event);
 })
+
+//Firebase stuff (hopefully works)
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDDZfcjCdkOOFwVReb0yK0VQL25h7S5yLY",
+    authDomain: "octothorpe-mocp.firebaseapp.com",
+    databaseURL: "https://octothorpe-mocp.firebaseio.com",
+    projectId: "octothorpe-mocp",
+    storageBucket: "octothorpe-mocp.appspot.com",
+    messagingSenderId: "1057063941890"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+$('#submit').on('click', function(){ 
+
+  // Retrieve user inputs from form
+  var hashtagName = $('#hashtag').val().trim();
+
+  var imageURL = shutterImageUrl
+
+  // Create an object for new hashtag to be added
+  var newHashtag = {
+    imageUrl: imageURL,
+    hashtagName: hashtagName
+  };
+
+    database.ref().push(newHashtag);
+  });
 
 
 
